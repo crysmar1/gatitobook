e22085c97daff0053e4416ee293f011a852df592
Cristiano Ribeiro <crysmar1@outlook.com>
2022-07-27 13:38:29 -0300

    Initial gatitobook course alura

---
+++ src/app/home/login/login.component.ts
@@ -0,0 +1,34 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  usuario='';
+  senha='';
+
+  constructor(private authService: AutenticacaoService, private router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  login(){
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      ()=> {
+      this.router.navigate(['animais']);
+      console.log('Autenticado com sucesso');
+      },
+      (error)=> {
+        alert('Usuário ou senha inválido');
+        console.log(error);
+      }
+    );
+    // console.log(this.usuario);
+    // console.log(this.senha);
+  }
+}
